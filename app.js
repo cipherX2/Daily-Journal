0be3164ae518328826263007ea40274d81fa207a
Dinesh <dineshpattnaik23@gmail.com>
2021-09-25 03:45:48 +0530

    Made compose page

--- app.js
+++ app.js
@@ -35,9 +35,13 @@ app.get('/contact', function (req, res) {
   });
 });
 
+app.get('/compose',function (req,res) {
+  res.render('compose');
+});
 
-
-
+app.post('/compose',function (req,res) {
+  console.log(req.body.composeText);
+});
 
 
 
