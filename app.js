305c3c6d81b6f3a82988a07b4605b17a92e451ba
Dinesh <dineshpattnaik23@gmail.com>
2021-09-25 03:32:46 +0530

    Make the navigation bar work

--- app.js
+++ app.js
@@ -17,11 +17,23 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get('/', (req,res) => {
-  res.render("home");
+app.get('/', function (req, res) {
+  res.render('home', {
+    homeContent: homeStartingContent
+  });
 });
 
+app.get('/about', function (req, res) {
+  res.render('about', {
+    aboutPageContent: aboutContent
+  });
+});
 
+app.get('/contact', function (req, res) {
+  res.render('contact', {
+    contactPageContent: contactContent
+  });
+});
 
 
 
