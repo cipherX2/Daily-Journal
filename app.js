353c5303c032b83fd2d61d40b5843feba7cdf8b9
Dinesh <dineshpattnaik23@gmail.com>
2021-09-25 23:51:23 +0530

    Implemented lodash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
@@ -43,7 +44,14 @@ app.get('/compose', function (req, res) {
 });
 
 app.get('/posts/:route', function (req, res) {
-  console.log(req.params.route);
+  // console.log(req.params.route);
+  for(let i=0; i<postContent.length; i++){
+    let routeData = _.lowerCase(req.params.route);
+    let  homeTitle = _.lowerCase(postContent[i].composeTitle);
+    if(routeData === homeTitle){
+      console.log("Match Found");
+    }
+  }
 });
 
 app.post('/compose', function (req, res) {
